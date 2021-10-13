a9aca7ba4b301ac8df5bb6ae01f5e145b65c59fd
Luis Andres Contreras <tremenditomen@gmail.com>
2021-10-12 18:31:53 -0600

    joining querys

---
+++ 2_queries/1_assaignment_duration.sql
@@ -0,0 +1,5 @@
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
+-- i couldn't figure this one out :/
