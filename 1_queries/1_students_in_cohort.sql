ebd590c05f08cd62340594cc41398d3b224309f8
Luis Andres Contreras <tremenditomen@gmail.com>
2021-11-22 19:37:10 -0700

    getting cohort 1 ids

--- 1_queries/1_students_in_cohort.sql
+++ 1_queries/1_students_in_cohort.sql
@@ -2,7 +2,7 @@
 -- FROM students
 -- ORDER BY  cohort_id ASC ,name ASC
 -- ^^^^this gives expected output bit its not the right answer.
-SELECT id, name 
+SELECT id AS student_id, name 
 FROM students 
 WHERE cohort_id = 1
 ORDER BY name;
